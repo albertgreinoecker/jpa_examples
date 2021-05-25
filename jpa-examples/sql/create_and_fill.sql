6dbdc95bdaf561ab5d658f9c063da8cddd37dfc1
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-05-25 22:19:43 +0200

    script to create and populate database

---
+++ jpa-examples/sql/create_and_fill.sql
@@ -0,0 +1,10 @@
+CREATE database uni_jpa;
+
+insert into STUDENT values (24002,'Xenokrates',18);
+insert into STUDENT values (25403,'Jonas',12);
+insert into STUDENT values (26120,'Fichte',10);
+insert into STUDENT values (26830,'Aristoxenos',8);
+insert into STUDENT values (27550,'Schopenhauer',6);
+insert into STUDENT values (28106,'Carnap',3);
+insert into STUDENT values (29120,'Theophrastos',2);
+insert into STUDENT values (29555,'Feuerbach',2);
