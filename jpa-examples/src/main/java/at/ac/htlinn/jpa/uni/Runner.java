bed2e86fe7762598e2b97780b8cf4d00a015de39
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-06-09 10:15:08 +0200

    fetch an "assistent" together with the boss

--- jpa-examples/src/main/java/at/ac/htlinn/jpa/uni/Runner.java
+++ jpa-examples/src/main/java/at/ac/htlinn/jpa/uni/Runner.java
@@ -17,8 +17,8 @@ public class Runner {
 		Student s = db.getStudent(25403);
 		System.out.println(s);
 		
-		s.setName("Jonas2.0");
-		s.setSemester(199);
+		s.setName("Jonas3.0");
+		s.setSemester(200);
 		db.aendereStudent(s);
 		s = db.getStudent(25403);
 		System.out.println(s);
@@ -29,6 +29,12 @@ public class Runner {
 		
 //		System.out.println("...und den löschen wir jetzt...");
 //		db.loescheStudent(25403);
+		
+		
+		Assistent ass = db.getAssistent(3003);
+		System.out.println(ass);
+		//Professor boss =
+		
 		
 		db.close();
 		System.out.println("Fertig!");
