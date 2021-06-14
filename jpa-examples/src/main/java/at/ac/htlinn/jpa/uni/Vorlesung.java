49b8b07d7275a6a0f55e759ef789a2cdde1dfaa9
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-06-14 20:51:08 +0200

    add vorlesung with relation to professor

---
+++ jpa-examples/src/main/java/at/ac/htlinn/jpa/uni/Vorlesung.java
@@ -0,0 +1,61 @@
+package at.ac.htlinn.jpa.uni;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Vorlesung {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long vorlNr;
+	private String titel;
+	private int ECTS; //European Credit Transfer and Accumulation System
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Professor gelesenVon;
+	
+	public Vorlesung() {
+	}
+
+	public long getVorlNr() {
+		return vorlNr;
+	}
+
+	public void setVorlNr(long vorlNr) {
+		this.vorlNr = vorlNr;
+	}
+
+	public String getTitel() {
+		return titel;
+	}
+
+	public void setTitel(String titel) {
+		this.titel = titel;
+	}
+
+	public int getECTS() {
+		return ECTS;
+	}
+
+	public void setECTS(int eCTS) {
+		ECTS = eCTS;
+	}
+
+	public Professor getGelesenVon() {
+		return gelesenVon;
+	}
+
+	public void setGelesenVon(Professor gelesenVon) {
+		this.gelesenVon = gelesenVon;
+	}
+
+	@Override
+	public String toString() {
+		return "Vorlesung [vorlNr=" + vorlNr + ", titel=" + titel + ", ECTS=" + ECTS + ", gelesenVon=" + gelesenVon
+				+ "]";
+	}
+	
+	
+}
